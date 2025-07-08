8379a6a7e976de7751db3843ca714345cc562d49
Vishnu Madala <vmadala@nxtleveltech.com>
2025-07-08 16:55:37 -0700

    feature2

--- src/main/java/com/empapp/employeemanagementbackend/EmployeemanagementbackendApplication.java
+++ src/main/java/com/empapp/employeemanagementbackend/EmployeemanagementbackendApplication.java
@@ -2,12 +2,18 @@ package com.empapp.employeemanagementbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class EmployeemanagementbackendApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(EmployeemanagementbackendApplication.class, args);
+    }
+    @GetMapping(value = "/")
+    public String hello() {
+        return "Hello World";
     }
 
 }
